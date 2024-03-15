b74ea622992440422597b6a1ba07009170eb6223
kikixt <kikixtt44@gmail.com>
2024-03-15 02:48:55 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,10 +8,13 @@ const ingredients = [
 ];
 
 const ingredientsList = document.getElementById("ingredients");
+const fragment = document.createDocumentFragment();
 
 ingredients.forEach(ingredient => {
   const listItem = document.createElement("li");
   listItem.textContent = ingredient;
   listItem.classList.add("item");
-  ingredientsList.appendChild(listItem);
+  fragment.appendChild(listItem);
 });
+
+ingredientsList.appendChild(fragment);
